05f304583ea920a51338d0dbbb3a83cbf1671cbc
Mark Marijnissen <markmarijnissen@gmail.com>
2014-08-04 13:41:46 +0200

    bugfix copy-url-loader limit query parameter

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
   MIT License http://www.opensource.org/licenses/mit-license.php
   Author Mark Marijnissen
 */
+var path = require('path');
 module.exports = function(content) {
   this.cacheable && this.cacheable();
   if(!this.emitFile) throw new Error("emitFile is required from module system");
   var url = this.resourcePath;
-  var root = this.query.substr(1) || this.options.copyContext || this.options.context;
+  var query = !query || query.substr(1,5) == "limit"? false: query.substr(1);
+  var root = query || this.options.copyContext || this.options.context;
   url = url.substr(url.indexOf(root) + root.length);
   this.emitFile(url, content);
+
+  console.log('url',root,this.resourcePath,url);
   return "module.exports = __webpack_public_path__ + " + JSON.stringify(url);
 };
 module.exports.raw = true;
